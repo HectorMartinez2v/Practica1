e4bf6893fc1c443174a51d07cc1bc9775b4813ec
HectorMartinez2v <hectormartinez2v@gmail.com>
2023-09-13 20:09:10 -0600

    Pide 2 numeros enteros

--- src/CODIGOS/Principal.java
+++ src/CODIGOS/Principal.java
@@ -1,10 +1,21 @@
 
 package CODIGOS;
 
+import java.util.*;
+
 /**
  *
  * @author Xploit
  */
 public class Principal {
+    
+    public static void main (String args[]){
+        System.out.println("Hola mundo");
+        
+        System.out.println("Digite 2 numeros enteros: ");
+        Scanner numero1= new Scanner (System.in);
+        Scanner numero2= new Scanner (System.in);
+        
+}
     
 }
